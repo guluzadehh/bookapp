71290664fdf42191a27b28e6acc937c9d8212186
guluzadehh <quluzade.resul13@mail.ru>
2025-01-04 03:59:04 +0400

    Add pkg logger init helper

--- pkg/sl/sl.go
+++ pkg/sl/sl.go
@@ -1,10 +1,17 @@
 package sl
 
-import "log/slog"
+import (
+	"log/slog"
+)
 
 func Err(err error) slog.Attr {
 	return slog.Attr{
 		Key:   "error",
 		Value: slog.StringValue(err.Error()),
 	}
+}
+
+func Init(log *slog.Logger, op string, requestId string) *slog.Logger {
+	log = log.With(slog.String("request_id", requestId))
+	return log.With("op", op)
 }
