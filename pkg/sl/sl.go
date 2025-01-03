fb9b3cd60ac66c5d51f0e48118101d51509a08dc
guluzadehh <quluzade.resul13@mail.ru>
2025-01-04 03:59:04 +0400

    Add pkg logger helper for handlers

--- pkg/sl/sl.go
+++ pkg/sl/sl.go
@@ -1,10 +1,23 @@
 package sl
 
-import "log/slog"
+import (
+	"log/slog"
+	"net/http"
+)
 
 func Err(err error) slog.Attr {
 	return slog.Attr{
 		Key:   "error",
 		Value: slog.StringValue(err.Error()),
 	}
+}
+
+type loggerSetter interface {
+	SetLog(log *slog.Logger)
+}
+
+func HandlerJob(log *slog.Logger, op string, r *http.Request, ls loggerSetter) *slog.Logger {
+	log = log.With(slog.String("request_id", r.Header.Get("X-Request-Id")))
+	ls.SetLog(log)
+	return log.With("op", op)
 }
